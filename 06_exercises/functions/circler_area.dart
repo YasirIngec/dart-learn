e68bf028c15ea7eafa08c42009c9d93e85b2bba1
Yasir <i.yasirr@hotmail.com>
2025-06-04 18:00:15 +0300

    Gereksiz dosyalar silindi ve launch.json güncellendi

--- 06_exercises/functions/circler_area.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-alan(int cap, [double pi = 3.14]) => cap * cap * pi;
-void main(List<String> args) {
-  stdout.write("Üçgenin yarı çapı:");
-  int cap = int.parse(stdin.readLineSync()!);
-
-  print("Üçgenin alanı :${alan(cap)}");
-}
